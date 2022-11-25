a3e24480f9e7fc7c2f55bf25b20801313c4768b9
arunmewada0805 <118459847+arunmewada0805@users.noreply.github.com>
2022-11-25 12:04:24 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,46 @@
+select * from dbo.['2018$']
+
+select * from dbo.['2019$']
+
+select * from dbo.['2020$']
+
+
+
+
+-- If revenue is increasing by Year?
+
+with hotels as (
+select * from dbo.['2018$']
+union
+select * from dbo.['2019$']
+union
+select * from dbo.['2020$'])
+
+select
+arrival_date_year,
+hotel,
+round(sum((stays_in_week_nights+stays_in_weekend_nights)*adr),2) as revenue
+from hotels
+group by arrival_date_year,hotel
+
+
+
+-- Market Segment
+
+select * from dbo.market_segment$
+
+--market segment and meal cost to year
+
+with hotels as (
+select * from dbo.['2018$']
+union
+select * from dbo.['2019$']
+union
+select * from dbo.['2020$'])
+
+select * from hotels
+left join dbo.market_segment$
+on hotels.market_segment = market_segment$.market_segment
+left  join
+dbo.meal_cost$
+on meal_cost$.meal = hotels.meal
